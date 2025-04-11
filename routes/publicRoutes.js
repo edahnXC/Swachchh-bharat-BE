9fc2418d4569a062e308ea76a187aa59c896e751
Om Kumar <edahnXC@gmail.com>
2025-04-11 21:25:22 +0530

    improved updates to render program

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   getCountries,
   getStates
 } = require("../controllers/adminController");
 
+const {
+  getPrograms
+} = require("../controllers/programController");  
+
 // Public routes for countries and states
 router.get("/countries", getCountries);
 router.get("/states", getStates);
+
+// Public route for programs
+router.get("/getPrograms", getPrograms); 
 
 module.exports = router;
